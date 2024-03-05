2542f71717a32432c48ed59f5d7686ee1a523999
Viren <144032304+viren15152@users.noreply.github.com>
2024-03-05 21:48:26 +0000

    Added the hrefs for my github and email to the Navbar.jsx file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/viren15152'
             >
               Github <FaGithub size={30} />
             </a>
@@ -110,7 +110,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='mailto:your-email@example.com'
             >
               Email <HiOutlineMail size={30} />
             </a>
@@ -129,3 +129,5 @@ const Navbar = () => {
   );
 };
 export default Navbar;
+
+//I will add the other hrefs once I've created a new Linkdin Profile and 
