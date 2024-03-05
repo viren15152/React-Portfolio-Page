b0b47a32eb003a87b05b3b5077b24ecb9bc1dc21
Viren <144032304+viren15152@users.noreply.github.com>
2024-03-05 18:59:49 +0000

    Added code for my Navbar.jsx file and Home.jsx file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import me from '../assets/Tiger.avif';
+import { Link } from "react-scroll"; 
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div> 
-  )
-}
-export default Home
+    <div
+    name="home"
+    className="h-screen w-full bg-[#0a192f]"
+  >
+    <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="flex flex-col justify-center h-full">
+        <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          My name is Viren Lala I'm currently training to become a Full Stack Web Developer. 
+        </h2>
+        <p className="text-gray-500 py-4 max-w-md">
+        I am currently a student learning Full Stack Web Development. My objective is to be the best web developer that I
+                    can be and to contribute to the technology industry. I am a detail
+                    oriented professional with a passion for problem solving and technological advancement. I continually search for ways to expand
+                    and improve my skill set. I recently started my journey in coding in Birmingham University Coding Boot Camp. Along with learning Full Stack Web Development I am also learning C++
+        </p>
+        <div>
+          <Link
+            to="about"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+          >
+            About Me
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight size={25} className="ml-3" />
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <img
+          src={me}  
+          alt="my profile"
+          className="rounded-2xl mx-auto w-2/3 md:w-full"
+        />
+      </div>
+    </div>
+  </div>
+  );
+};
+export default Home;
