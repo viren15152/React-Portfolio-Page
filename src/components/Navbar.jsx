75f98cbce0a911bbeca9871260bd0c459c42b911
Viren <144032304+viren15152@users.noreply.github.com>
2025-01-27 21:29:43 +0000

    Added Linkedin link to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,7 +94,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/viren-lala-78a243313/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
