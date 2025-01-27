04379ba85b6e963f98f690d94d26c3e09422e504
Viren <144032304+viren15152@users.noreply.github.com>
2025-01-27 13:24:50 +0000

    Added latest CV to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -118,7 +118,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/Viren Mahesh Lala CV.docx'
+              href='/(Mr Viren Mahesh Lala) Full-Stack Web Development CV.docx'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
